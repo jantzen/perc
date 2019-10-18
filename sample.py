334b7ae8e2190031c0dc959250877bdbc6ccc062
Benjamin C. Jantzen <jantzen@ratiocination.org>
2019-10-18 15:15:41 -0400

    Sketched Python sampling script.

---
+++ sample.py
@@ -0,0 +1,28 @@
+# file: sample.py
+
+import numpy as np
+
+
+def main(p_set, soil_type, p_tol=0.01, sampling_rate=100.):
+    pass
+
+    # pass pressure parameters to Arduino Nano
+
+    # confirm receipt
+
+    # confirm depth can be acquired from Arduino Uno
+
+    # confirm temperature sensor can be read
+
+    # loop
+        # read depth, pressure, and temperature every 0.01 seconds
+
+    # save data
+        # filename should contain: p_set, p_tol, soil_type, sampling rate,
+        # start_time, end_time
+
+
+if __name__ == __main__:
+    # read command line arguments
+        # require: pressure setpoint, soil type
+    main()
